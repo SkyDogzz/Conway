60dda7e8834018a6c24d89b129a28061ac0215df
Thomas Stephan <tstephan@f2r11s16.clusters.42paris.fr>
2025-05-26 11:43:29 +0200

    dont segfault if trying to write out of img

--- src/mlx/img.c
+++ src/mlx/img.c
@@ -3,6 +3,10 @@
 void my_mlx_pixel_put(t_data *data, int x, int y, int color) {
 	char *dst;
 
+	if (x < 0 || x > WINDOW_WIDTH)
+		return ;
+	if (y < 0 || y > WINDOW_HEIGHT)
+		return ;
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
